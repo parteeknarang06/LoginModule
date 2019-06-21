4774c85df93758e1047e90b871268f8a16cc0098
parteeknarang06 <parteeknarang06@gmail.com>
2019-06-21 18:16:21 +0530

    21-June-2019
    21-June-2019

--- src/com/model/action/Login.java
+++ src/com/model/action/Login.java
@@ -1,13 +1,24 @@
 package com.model.action;
 
+import java.util.Random;
+
 import org.apache.log4j.Logger;
 
 public class Login {
 	private static final Logger logger ;
-	
+	private StringBuilder token;
 	static {
 		logger = Logger.getLogger(Login.class);
 	}
 	
+	public void createToken(String sessionId) {
+		Random random=new Random();
+		token = new StringBuilder();
+		do {
+			int randomNum=random.nextInt(sessionId.length());
+			char sessionChar = sessionId.charAt(randomNum); 
+			token.append(String.valueOf(randomNum)+sessionChar);
+		} while(token.length()<17);
+	}
 	
 }
--- src/com/model/service/LoginHandler.java
+++ src/com/model/service/LoginHandler.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
 
 import com.model.common.NameTagParam;
 import com.model.common.RowColStore;
+import com.model.action.Login;
 import com.model.common.LoggerUtility;
 import com.model.config.workflows.ActionResult;
 import com.model.config.workflows.WorkflowActionResult;
@@ -19,12 +20,14 @@ public class LoginHandler {
 	public ActionResult checkLoginStatus(HttpServletRequest request,HttpServletResponse response,RowColStore rcs,NameTagParam ntp) {
 		rcs.dropAllRows();
 		LoggerUtility.logInfo(logger, request, "Jsessionid size:"+request.getSession().getId().length());
+		
 		return new WorkflowActionResult("NEED_LOGIN");
 	}
 	
 	public ActionResult logMeIn(HttpServletRequest request,HttpServletResponse response,RowColStore rcs,NameTagParam ntp) {
 		rcs.dropAllRows();
-		
+		Login login = new Login();
+		login.createToken(request.getSession().getId());
 		return new WorkflowActionResult("LOGIN_OK");
 	}
 }
